e9964cfe16b628ed166bd509867be382ba556b8d
LucasLvy <lucaslevy10@gmail.com>
2021-03-29 19:08:58 +0200

    Style douteux

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import { song } from './abis/songabi';
 import { toudou } from './abis/toudouabi';
+import { title } from 'process';
 const web3 = new Web3(Web3.givenProvider)
 const address_song = '0x004a84209A0021b8FF182FfD8BB874c53F65e90E'
 const song_sc = new web3.eth.Contract(song, address_song)
 const address_toudou = '0x89150a0325ecc830a2304a44de98551051b4f466'
 const toudou_sc = new web3.eth.Contract(toudou, address_toudou)
+const url = require('url');
 
 class App extends Component {
   componentWillMount() {
@@ -27,8 +29,9 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    console.log(window.location.href.split("=")[1])
+    const accounts =  window.location.href.split("=")[1] || await web3.eth.getAccounts()
+    this.setState({ account: accounts })
     const chainID = await web3.eth.getChainId()
     this.setState({chainID})
     const lastBlock = await web3.eth.getBlockNumber()
@@ -69,6 +72,7 @@ class App extends Component {
     })
   }
 
+
   async displayTokens() {
     var maxid = await toudou_sc.methods.totalSupply().call()
     var owned = []
@@ -92,9 +96,6 @@ class App extends Component {
   }
 
   transfer(contrat) {
-    console.log(this.state.account)
-    console.log(this.state.destination)
-    console.log(this.state.formid)
     contrat.methods.safeTransferFrom(this.state.account,this.state.destination,this.state.formid).send({from:this.state.account,to:this.state.destination,tokenId:this.state.formid})
     .once('receipt', (receipt) => {
       this.setState({ receipt })
@@ -151,17 +152,27 @@ class App extends Component {
     console.log(target.name)
     this.setState({[target.name]: target.value})
   }
+  
   render() {
+    const titleStyle = {
+      marginLeft:'5px',
+      color: '#02859F',
+      fontSize: '30px',
+      fontWeight: "bold",
+    };
+     const elementStyle={
+       marginLeft:'10px'
+     }
     return (
       <div className="container">
-        <h1>Trop bien ce site</h1>
-        <p>Your account: {this.state.account}</p>
-        <p>Chain id: {this.state.chainID}</p>
-        <p>Last block: {this.state.lastBlock}</p>
-        <p>Number of tokens: {this.state.nftnumber}</p>
-        <p>Name of tokens: {this.state.nftname}</p>
-        <p>Name of the token: {this.state.tokenURI.name}</p>
-        <p>Description of the tokens: {this.state.tokenURI.description}</p>
+        <h1 style={{fontSize:'70px',marginLeft:'-10px'}}>Trop bien ce site</h1>
+        <p style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Your account: </div> <div style={elementStyle}>{this.state.account}</div></p>
+        <p style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Chain id: </div><div style={elementStyle}>{this.state.chainID}</div></p>
+        <p style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Last block:  </div><div style={elementStyle}>{this.state.lastBlock}</div></p>
+        <p style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Number of tokens: </div> <div style={elementStyle}>{this.state.nftnumber}</div></p>
+        <p style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Name of tokens:  </div><div style={elementStyle}>{this.state.nftname}</div></p>
+        <p style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Name of the token:  </div><div style={elementStyle}>{this.state.tokenURI.name}</div></p>
+        <p style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Description of the tokens:  </div><div style={elementStyle}>{this.state.tokenURI.description}</div></p>
         <img src={this.state.tokenURI.img} alt="new" style={{width:'50%'}}/>
         <button onClick={(event) => {
           event.preventDefault()
@@ -171,13 +182,13 @@ class App extends Component {
           event.preventDefault()
           toudou_sc.methods.buyAToken().send({ from: this.state.account, value: 2*10**17 })
         }}>Buy a new tout doucement token</button>
-        <p>You have {this.state.nbOwnedSong} song token with those IDs :</p>
+        <p><div style={titleStyle,{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}>You have </div><div style={{textDecorationLine: 'underline'}}> {this.state.nbOwnedSong} song token with those IDs : </div> </p>
         <div>
           {this.state.ownedSong.map(token => (
             <p>~ {token}</p>
           ))}
         </div>
-        <p>You have {this.state.nbOwnedToudou} tout doucement token with those IDs:</p>
+        <p><div style={titleStyle,{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}>and </div><div style={{textDecorationLine: 'underline'}}>{this.state.nbOwnedToudou} tout doucement token with those IDs:</div></p>
         <div>
           {this.state.ownedToudou.map(token => (
             <p>~ {token}</p>
@@ -188,34 +199,39 @@ class App extends Component {
           event.preventDefault()
           this.transfer(song_sc)
         }}>
-          <p>Transfer song token :</p>
+          <div style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}><div style={titleStyle}>Transfer song token :</div> 
           <input id="newID" ref={this.state.formid} name="formid" type="number" className="form-control" onChange={this.handleInputChange} placeholder="ID" required />
           <input id="destination" ref={this.state.destination} name="destination" type="text" className="form-control" onChange={this.handleInputChange} placeholder="Destination address" required />
           <input type="submit" />
+          </div>
         </form>
         <form onSubmit={(event) => {
           event.preventDefault()
           this.transfer(toudou_sc)
         }}>
-          <p>Transfer Tout Doucement token :</p>
+          <div style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",marginLeft:'5px',
+      color: '#02859F',}}><div style={titleStyle}>Transfer Tout Doucement token :</div>
           <input id="newID" ref={this.state.formid} type="number" className="form-control" onChange={this.handleInputChange} placeholder="ID" required />
           <input id="newDestination" ref={this.state.destination} type="text" className="form-control" onChange={this.handleInputChange} placeholder="Destination address" required />
-          <input type="submit" />
+          <input type="submit" /></div>
       </form>
-      
-        <p><br/>List of song token owners :</p>
-        <div>
+        <div style = {{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",marginLeft:'10px'}}>
+        <div style={titleStyle}> List of song token owners :</div>
+        <div style={{marginLeft:'20px'}}>
           {this.state.ownersOfSong.map(token => (
             <p>{token}</p>
           ))}
         </div>
-        <p><br/>List of Tout doucement token owners :</p>
-        <div>
-          {this.state.ownersOfToudou.map(token => (
-            <p>{token}</p>
-          ))}
         </div>
-      </div>
+        <div style={{borderRadius: 10,boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",marginLeft:'10px'}}>
+          <div style = {titleStyle}>List of Tout doucement token owners :</div>
+          <div style={{marginLeft:'20px'}}>
+            {this.state.ownersOfToudou.map(token => (
+              <p>{token}</p>
+            ))}
+          </div>
+        </div>
+        </div>
     );
   }
 }
